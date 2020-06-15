ee081ce28d542dfa51c5e1f58d71719fe6b80e2a
Truc Anh Nguyen Phan <trucphan307@gmail.com>
2020-06-15 20:36:22 +0200

    Add firebase config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import Phaser from 'phaser';
 import config from './config/config';
+import firebaseConfig from './config/firebaseConfig';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 import ReceptionScene from './scenes/ReceptionScene';
 import TrainingScene from './scenes/TrainingScene';
 import PhoneScene from './scenes/PhoneScene';
@@ -14,6 +17,18 @@ class Game extends Phaser.Game {
 
 		// Initialize the game on the first scene
 		this.scene.start('Training');
+	}
+
+	create () {
+		const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+		 // Initialize Cloud Firestore through Firebase
+		var db = firebaseApp.firestore();
+		db.settings({
+			timestampsInSnapshots: true
+		})
+		console.log('AAAAHHHH')
+		console.log(db)
 	}
 }
 
